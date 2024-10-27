01445cd1a261649ef48a0f4b83bc58c477ef5254
AndreyMaraj <andrey_maraj@icloud.com>
2024-10-27 17:25:31 +0300

    #35. Добавить страницу администрирования заказов

--- app/administration/orders/[id]/page.tsx
+++ app/administration/orders/[id]/page.tsx
@@ -6,11 +6,11 @@ import EmptyProductMedia from '@/public/empty-product-media.jpg'
 export default async function ({ params }: Omit<PageProps<'id', never>, 'searchParams'>) {
 	const session = await auth()
 
-	if (!session?.user.id) {
+	if (!session || !session.user.id || session.user.role !== 'ADMIN') {
 		return
 	}
 
-	const order = await getOrder(Number(params.id)),
+	const order = await getOrder(Number(params.id), true),
 		orderItems = order?.orderItems.map(item => ({
 			id: item.invisibleModelModification.id,
 			count: item.count,
@@ -23,6 +23,9 @@ export default async function ({ params }: Omit<PageProps<'id', never>, 'searchP
 			<h1 className='text-3xl mb-5'>
 				Заказ от {order?.date.toLocaleDateString('ru-RU')}
 			</h1>
+			<div>
+				Пользователь: {order?.user.name} {order?.user.tin} {order?.user.city}
+			</div>
 			<div>
 				<ProductsTable rows={orderItems} />
 			</div>
