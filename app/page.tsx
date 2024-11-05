4e058f6e4517e2123f5217dc54f5a671a827e3fe
AndreyMaraj <andrey_maraj@icloud.com>
2024-11-05 12:33:41 +0300

    Обновление политики безопсаности и фото главной страницы

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
 import Carousel from '@/components/carousel'
-import Slider1 from '@/public/slider-1.png'
-import Slider2 from '@/public/slider-2.png'
-import Slider3 from '@/public/slider-3.png'
-import Slider4 from '@/public/slider-4.png'
-import Slider5 from '@/public/slider-5.jpg'
-import Slider6 from '@/public/slider-6.jpg'
-import Slider7 from '@/public/slider-7.jpg'
-import Slider8 from '@/public/slider-8.jpg'
-import Slider9 from '@/public/slider-9.jpg'
-import Slider10 from '@/public/slider-10.jpg'
-import Slider11 from '@/public/slider-11.jpg'
-import Slider12 from '@/public/slider-12.jpg'
-import Slider13 from '@/public/slider-13.jpg'
+import Slider1 from '@/public/slider-18.jpg'
+import Slider2 from '@/public/slider-19.jpg'
+import Slider3 from '@/public/slider-20.jpg'
+import Slider4 from '@/public/slider-21.jpg'
+import Slider5 from '@/public/slider-14.jpg'
+import Slider6 from '@/public/slider-15.jpg'
+import Slider7 from '@/public/slider-16.jpg'
+import Slider8 from '@/public/slider-17.jpg'
 import { Button, Image } from '@nextui-org/react'
 import NextImage from 'next/image'
 import DiamondIcon from '@/public/diamond-icon.svg'
@@ -47,21 +42,6 @@ export default function () {
 						alt: ''
 					}, {
 						src: Slider8.src,
-						alt: ''
-					}, {
-						src: Slider9.src,
-						alt: ''
-					}, {
-						src: Slider10.src,
-						alt: ''
-					}, {
-						src: Slider11.src,
-						alt: ''
-					}, {
-						src: Slider12.src,
-						alt: ''
-					}, {
-						src: Slider13.src,
 						alt: ''
 					}]} hideArrows className='flex-1' />
 					<div className='flex flex-col flex-1 gap-y-10'>
